75012106838260bcaafb9754779b6dedf4ad3d14
Mac <hatanduy123@gmail.com>
2023-11-22 23:29:03 +0700

    doneJWT

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Res,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { serialize } from 'cookie';
 
 @Controller('users')
 export class UsersController {
@@ -20,13 +21,31 @@ export class UsersController {
 
   @Post('/login')
   @HttpCode(HttpStatus.OK)
-  async logInUser(@Body() body: any) {
+  async logInUser(@Body() body, @Res() res) {
     const { email, password } = body;
-    return await this.service.logIn(email, password);
+    const token = await this.service.logIn(email, password);
+    res.setHeader(
+      'Set-Cookie',
+      serialize('accessToken', token?.access_token, {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+      }),
+    );
+    return res.json({ success: true });
   }
 
   @Post('/register')
-  async insertDataUser(@Body() body: any) {
-    return await this.service.reGisTer(body);
+  async insertDataUser(@Body() body, @Res() res) {
+    const token = await this.service.reGisTer(body);
+    res.setHeader(
+      'Set-Cookie',
+      serialize('accessToken', token?.access_token, {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+      }),
+    );
+    return res.json({ success: true });
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,13 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '10m' },
     }),
   ],
   controllers: [UsersController],
